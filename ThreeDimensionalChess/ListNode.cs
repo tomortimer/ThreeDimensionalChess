9f1cdeb2c0d1d6fed3a1488f16cf7d9baa82ee6d
tomortimer <k036952@eltham-college.org.uk>
2023-05-17 12:44:22 +0100

    Added Data Structures from Data Structure project; Started setting up object structure for project

---
+++ ThreeDimensionalChess/ListNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class ListNode<T>
+    {
+        private T data;
+        public ListNode<T> next = null;
+
+        //constructor
+        public ListNode(T inp)
+        {
+            data = inp;
+        }
+
+        //get set methods
+        public T GetData() { return data; }
+        public void SetData(T inp) { data = inp; }
+    }
+}
