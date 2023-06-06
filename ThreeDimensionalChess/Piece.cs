bce5d8385ac1d014368e7a01947783ba6503a2ca
tomortimer <k036952@eltham-college.org.uk>
2023-06-06 12:29:57 +0100

    Added some methods to Piece class, should be ready to start creating child classes soon.

--- ThreeDimensionalChess/Piece.cs
+++ ThreeDimensionalChess/Piece.cs
@@ -20,13 +20,20 @@ namespace ThreeDimensionalChess
             currentPosition = startPos;
         }
 
-        //method to calculate all possible moves by a piece - returns a list
-        public List<int> generatePossibleMoves()
+        //method to calculate all possible moves by a piece - returns a list, must be implemented on a per piece basis
+        abstract public List<int> generatePossibleMoves();
+
+        private int convertVectToPtr(int[] arr)
         {
-            List<int> moves = new List<int>();
+            // if this method doesn't receive a three dimensional vector in array form, it will throw an argument exception - thankfully this should never happen
+            if(arr.Length != 3){ throw new ArgumentException(); }
 
-
-            return moves;
+            // uses base 8 system to convert three dimensional vectors into pointers for the list of board squares
+            int movePtr = 0;
+            movePtr += arr[0];
+            movePtr += arr[1] * 8;
+            movePtr += arr[2] * 64;
+            return movePtr;
         }
     }
 }
